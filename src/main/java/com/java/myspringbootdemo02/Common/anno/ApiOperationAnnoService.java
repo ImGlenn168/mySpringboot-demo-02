f4b3522772e5abe4b0cc95be6308f9be2ff2194b
tanlin123 <534606744@qq.com>
2023-09-11 16:03:20 +0800

    注解实现日志功能优化 --Glenn--20230911

--- src/main/java/com/java/myspringbootdemo02/Common/anno/ApiOperationAnnoService.java
+++ src/main/java/com/java/myspringbootdemo02/Common/anno/ApiOperationAnnoService.java
@@ -2,18 +2,22 @@ package com.java.myspringbootdemo02.Common.anno;
 
 import com.java.myspringbootdemo02.Common.vo.ApiOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.lang.reflect.Method;
 
 @Slf4j
 @Service
 public class ApiOperationAnnoService {
+    
+    @Value("${anno.aClass}")
+    private String aClass;
 
     public ApiOperator buildApiOperationLog(ApiOperator apiOperator) {
             synchronized (ApiOperationAnnoService.class) {
                 try {
                     // 检查注解的类
-                    Class<?> aClass = Class.forName("com.java.myspringbootdemo02.Api.controller.user.impl.UserControllerImpl");
+                    Class<?> aClass = Class.forName(this.aClass);
                     Method[] declaredMethods = aClass.getDeclaredMethods();
                     for (Method declaredMethod : declaredMethods) {
                         log.info("current method : {}", declaredMethod);
